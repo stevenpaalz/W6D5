fcbbe8cfabd18c6c6ff911af0a6df8a68f178864
Olga <olga.sleepless@gmail.com>
2023-02-17 17:07:51 -0500

    Add Cat class

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,15 @@
+require 'date'
+class Cat < ApplicationRecord
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :sex, inclusion: {in: ['M','F']}
+  validate :birth_date_cannot_be_future
+
+  CAT_COLORS = ['brown','black','white']
+  validates :color, inclusion: {in: CAT_COLORS}
+
+  def birth_date_cannot_be_future
+    if self.birth_date > Date.today
+      errors.add(:birth_date, "can't be in the future")
+    end
+  end
+end
